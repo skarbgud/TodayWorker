f30028b6d2265f458832c03dbf7764f1e58eac97
skarbgud1 <sjarbgud@gmail.com>
2021-05-05 21:14:46 +0900

    프로젝트 세팅

---
+++ src/test/java/com/workerholic/controller/DataSourceTest.java
@@ -0,0 +1,29 @@
+package com.workerholic.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class DataSourceTest {
+
+    @Inject
+    private DataSource ds;
+
+    @Test
+    public void testConnection() throws Exception{
+        try {
+            Connection con = ds.getConnection();
+            System.out.println(con);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
