8155d4cc12ee8de7eea7bbd7b3900b1db760d8b3
sjarbgud <sjarbgud@gmail.com>
2021-07-31 18:38:09 +0900

    [FE] 메인화면 Content

--- frontend/src/views/frame/components/FrameSection.vue
+++ frontend/src/views/frame/components/FrameSection.vue
@@ -1,14 +1,135 @@
 <template>
-<div>
-  
-</div>
+  <div>
+    <b-container fluid>
+      <b-row align-h="center" class="mt-5">
+        <b-col cols="8">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="인기게시물" align="center">
+              <b-card-text>인기게시물 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+      <b-row align-h="center" class="mt-5">
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="자유게시판" align="center">
+              <b-card-text>자유게시판 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="비밀게시판" align="center">
+              <b-card-text>비밀게시판 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+      <b-row align-h="center" class="mt-5">
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="사는얘기" align="center">
+              <b-card-text>사는얘기 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="이직, 커리어" align="center">
+              <b-card-text>이직, 커리어 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+      <b-row align-h="center" class="mt-5">
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="회사생활" align="center">
+              <b-card-text>회사생활 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="취미생활" align="center">
+              <b-card-text>취미생활 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+      <b-row align-h="center" class="mt-5">
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="시사.이슈" align="center">
+              <b-card-text>시사.이슈 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="정보게시판" align="center">
+              <b-card-text>정보게시판 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+      <b-row align-h="center" class="mt-5">
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="커피타임" align="center">
+              <b-card-text>커피타임 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="홍보게시판" align="center">
+              <b-card-text>홍보게시판 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+      <b-row align-h="center" class="mt-5">
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="맛집노트" align="center">
+              <b-card-text>맛집노트 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="중고거래" align="center">
+              <b-card-text>중고거래 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+      <b-row align-h="center" class="mt-5 mb-5">
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="동호회" align="center">
+              <b-card-text>동호회 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+        <b-col cols="4">
+          <b-card-group deck>
+            <b-card border-variant="dark" header="유머" align="center">
+              <b-card-text>유머 내용</b-card-text>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'FrameSection',
-  components: {
-  },
+  components: {},
 };
 </script>
 
