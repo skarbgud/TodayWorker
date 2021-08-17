454dbfcca4bd0094fb576b100a1b59969f6754f6
wlsgml0229@naver.com <wlsgml0229@naver.com>
2021-08-17 23:15:58 +0900

    [FE] Swiper 적용 (TODO. 간격조절, 페이지네이션)

--- frontend/src/views/components/cards/CardList.vue
+++ frontend/src/views/components/cards/CardList.vue
@@ -1,53 +1,51 @@
 <template>
- <div>
-  <b-card :title="post.title" class="card-link">
-
+  <div>
+    <b-card :title="post.title" class="card-link">
       <b-card-text class="mt-4 mb-4">
-        <b-link  class="card-link">
-        {{post.content}}
+        <b-link class="card-link">
+          {{ post.content }}
         </b-link>
       </b-card-text>
 
-    <!--유저 정보-->
-    <div class="user-info">
+      <!--유저 정보-->
+      <div class="user-info">
         <a href="#" class="card-link">삼성전자</a>
-        <b-link href="#" class="card-link">m****</b-link><br>
-    </div>
+        <b-link href="#" class="card-link">m****</b-link><br />
+      </div>
 
-    <!-- 조회수, 좋아요, 댓글 갯수-->
-    <div class="icon-color mt-1">
-    <b-icon class="ml-0 " icon="hand-index-thumb" /> 505
-     <b-icon class="mx-2 ml-3 " icon="hand-thumbs-up"/>좋아요
-     <b-icon class="mx-2 ml-3 " icon="chat"/>38
-    </div>
-  </b-card>
-</div>
+      <!-- 조회수, 좋아요, 댓글 갯수-->
+      <div class="icon-color mt-1">
+        <b-icon class="ml-0 " icon="hand-index-thumb" /> 505
+        <b-icon class="mx-2 ml-3 " icon="hand-thumbs-up" />좋아요
+        <b-icon class="mx-2 ml-3 " icon="chat" />38
+      </div>
+    </b-card>
+  </div>
 </template>
 
 <script>
 export default {
-    props:{
-     post: {
+  name: 'CardList',
+  props: {
+    post: {
       type: Object,
     },
-    },
-    data() {
-        return {
-            
-        }
-    }
-}
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-.icon-color{
-    color:rgb(145, 141, 141);
-    font-size: 14px;
+.icon-color {
+  color: rgb(145, 141, 141);
+  font-size: 14px;
 }
-a{
- color: black;
+a {
+  color: black;
 }
 .user-info {
-    font-size: 14px;
+  font-size: 14px;
 }
 </style>
--- frontend/src/views/components/navBar.vue
+++ frontend/src/views/components/navBar.vue
@@ -1,34 +1,87 @@
 <template>
   <div>
-      
-    <b-nav>
-        <b-nav-item v-for="(title,index) in cardTitles" :key="index" class="item" @click="golive(index)">
-            {{cardTitles[index].title}}
-        </b-nav-item>
-  </b-nav>
-</div>
+    <b-row class="mb-4">
+      <swiper class="swiper" :options="swiperOption">
+        <div class="swiper-pagination" slot="pagination"></div>
+        <swiper-slide
+          v-for="(title, index) in cardTitles"
+          :key="index"
+          class="item"
+          @click="golive(index)"
+        >
+          {{ cardTitles[index].title }}
+          <!-- <b-nav>
+          <b-nav-item
+            v-for="(title, index) in cardTitles"
+            :key="index"
+            class="item"
+            @click="golive(index)"
+          >
+            {{ cardTitles[index].title }}
+          </b-nav-item>
+        </b-nav> -->
+        </swiper-slide>
+      </swiper>
+    </b-row>
+  </div>
 </template>
 
 <script>
-import cardTitles from '@/constant/index';
+import cardTitles from "@/constant/index";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 
 export default {
-    data() {
-        return{
-            cardTitles,
-        }
-    },
-     methods: {
-    golive(index) {
-            console.log(this.cardTitles[index]);
-            this.$router.push(this.cardTitles[index].path).catch(() => {});
+  name: "NavBar",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      cardTitles,
+      swiperOption: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          1024: {
+            slidesPerView: 8,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+        },
       },
-    }
-}
+    };
+  },
+  methods: {
+    golive(index) {
+      console.log(this.cardTitles[index]);
+      this.$router.push(this.cardTitles[index].path).catch(() => {});
+    },
+  },
+};
 </script>
 
 <style>
 .item:hover {
- border-bottom: 2px solid #0b0c0c;
+  border-bottom: 2px solid #0b0c0c;
+}
+.swiper-pagination{
+    /* clear: both; */
 }
 </style>
