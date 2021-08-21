e5e70d98191d12465999c45991ab9792541fcaad
sjarbgud <sjarbgud@gmail.com>
2021-08-21 20:13:54 +0900

    [FE] 모달창 소셜 아이콘 png로 대체

--- frontend/src/views/components/modal/LoginModal.vue
+++ frontend/src/views/components/modal/LoginModal.vue
@@ -54,54 +54,32 @@
           width="18"
           height="18"
           viewBox="0 0 18 18"
-          src="@/assets/naver.png"
+          src="@/assets/icon/naver.png"
         />
         네이버로 시작하기
       </button>
       <button class="social-login-button">
-        <svg
+        <img
           class="icon-image"
-          xmlns="http://www.w3.org/2000/svg"
           width="18"
           height="18"
           viewBox="0 0 18 18"
-        >
-          <path
-            fill="#1877f2"
-            fill-rule="evenodd"
-            d="M18 9a9 9 0 1 0-10.406 8.89v-6.288H5.309V9h2.285V7.017c0-2.255 1.343-3.501 3.4-3.501.984 0 2.014.175 2.014.175v2.215h-1.135c-1.118 0-1.467.694-1.467 1.406V9h2.496l-.399 2.602h-2.097v6.289C14.71 17.215 18 13.492 18 9"
-          ></path></svg
-        >페이스북으로 시작하기
+          src="@/assets/icon/facebook.png"
+        />
+        페이스북으로 시작하기
       </button>
+
       <button class="social-login-button">
-        <svg
+        <img
           class="icon-image"
-          xmlns="http://www.w3.org/2000/svg"
           width="18"
           height="18"
           viewBox="0 0 18 18"
-        >
-          <g fill="none" fill-rule="evenodd">
-            <path
-              fill="#EA4335"
-              d="M9 3.48c1.69 0 2.83.73 3.48 1.34l2.54-2.48C13.46.89 11.43 0 9 0 5.48 0 2.44 2.02.96 4.96l2.91 2.26C4.6 5.05 6.62 3.48 9 3.48z"
-            ></path>
-            <path
-              fill="#4285F4"
-              d="M17.64 9.2c0-.74-.06-1.28-.19-1.84H9v3.34h4.96c-.1.83-.64 2.08-1.84 2.92l2.84 2.2c1.7-1.57 2.68-3.88 2.68-6.62z"
-            ></path>
-            <path
-              fill="#FBBC05"
-              d="M3.88 10.78A5.54 5.54 0 0 1 3.58 9c0-.62.11-1.22.29-1.78L.96 4.96A9.008 9.008 0 0 0 0 9c0 1.45.35 2.82.96 4.04l2.92-2.26z"
-            ></path>
-            <path
-              fill="#34A853"
-              d="M9 18c2.43 0 4.47-.8 5.96-2.18l-2.84-2.2c-.76.53-1.78.9-3.12.9-2.38 0-4.4-1.57-5.12-3.74L.97 13.04C2.45 15.98 5.48 18 9 18z"
-            ></path>
-            <path d="M0 0h18v18H0z"></path>
-          </g></svg
-        >Google로 시작하기
+          src="@/assets/icon/google.png"
+        />
+        Google로 시작하기
       </button>
+
       <p class="footer-info">
         걱정마세요! 여러분의 활동은 SNS에 노출되지 않습니다.
       </p>
