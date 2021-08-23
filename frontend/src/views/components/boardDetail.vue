ff8561638b97370011127a8d1df1572067503804
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2021-08-24 00:59:27 +0900

    [FE] 상세페이지 - 댓글영역추가

--- frontend/src/views/components/boardDetail.vue
+++ frontend/src/views/components/boardDetail.vue
@@ -6,57 +6,71 @@
         <b-row>
           <b-col cols="8" class="mt-4">
             <h2>{{ post.title }}</h2>
-            <div class="userbox mt-3">
-              <div class="user-info  mr-2">
-                {{ user.do }}
-                {{ user.si }} |
-                {{ user.userId }}
-                {{ user.company }}
+            <div class="content">
+              <div class="userbox mt-3">
+                <div class="user-info  mr-2">
+                  {{ user.do }}
+                  {{ user.si }} |
+                  {{ user.userId }}
+                  {{ user.company }}
+                </div>
+                <div>
+                  <a href="#">
+                    <b-icon
+                      icon="bell-fill"
+                      class="rounded-circle bg-primary  p-1"
+                      variant="light"
+                  /></a>
+                </div>
               </div>
-              <div>
-                <a href="#">
-                  <b-icon
-                    icon="bell-fill"
-                    class="rounded-circle bg-primary  p-1"
-                    variant="light"
+              <div class="post-info mt-1">
+                <b-icon class="mx-1" icon="clock" />2시간전
+                <b-icon class=" ml-2 " icon="hand-index-thumb" /> 505
+                <b-icon class="mx-1 ml-2" icon="chat" />38
+                <div class="info-right">
+                  <a href="#"><b-icon class="mx-1 ml-3" icon="bookmark"/></a>
+                  <a href="#"><b-icon class="mx-1 ml-3" icon="three-dots"/></a>
+                </div>
+              </div>
+              <hr />
+              <div class="mt-4 mb-4">{{ post.content }}</div>
+              <div class="info-bottom">
+                <a href="#"
+                  ><b-icon class="mx-1 ml-2" icon="hand-thumbs-up" />505</a
+                >
+                <a href="#"><b-icon class="mx-1 ml-2" icon="chat" />38</a>
+                <a href="">
+                  <img
+                    src="https://storage.googleapis.com/storage.chris-chris.ai/images/facebook.gif"
+                    title="페이스북으로 공유하기"
+                    class="sharebtn_custom"
+                /></a>
+                <a href=""
+                  ><img
+                    src="https://storage.googleapis.com/storage.chris-chris.ai/images/twitter.gif"
+                    title="트위터로 공유하기"
+                    class="sharebtn_custom"
+                /></a>
+                <a href=""
+                  ><img
+                    src="https://storage.googleapis.com/storage.chris-chris.ai/images/story.gif"
+                    title="카카오스토리로 공유하기"
+                    class="sharebtn_custom"
                 /></a>
               </div>
+              <hash-tag :hashTag="post.hashTag" />
+              <div style="clear:both" />
+              <hr />
             </div>
-            <div class="post-info mt-1">
-              <b-icon class="mx-1" icon="clock" />2시간전
-              <b-icon class=" ml-2 " icon="hand-index-thumb" /> 505
-              <b-icon class="mx-1 ml-2" icon="chat" />38
-              <div class="info-right">
-                <a href="#"><b-icon class="mx-1 ml-3" icon="bookmark"/></a>
-                <a href="#"><b-icon class="mx-1 ml-3" icon="three-dots"/></a>
+            <div class="article-comments pt-2">
+              <h6>댓글 127</h6>
+              <b-link>
+              <div class="comment-box mt-2">
+               <b-icon icon="camera" font-scale="2"></b-icon >
+                <span class="comment">댓글을 입력하세요</span>
               </div>
+              </b-link>
             </div>
-            <hr />
-            <div class="mt-4 mb-4">{{ post.content }}</div>
-            <div class="info-bottom">
-               <a href="#"><b-icon class="mx-1 ml-2" icon="hand-thumbs-up"/>505</a>
-              <b-icon class="mx-1 ml-2" icon="chat" />38
-
-              <a href="">
-                <img
-                  src="https://storage.googleapis.com/storage.chris-chris.ai/images/facebook.gif"
-                  title="페이스북으로 공유하기"
-                  class="sharebtn_custom"
-              /></a>
-              <a href=""
-                ><img
-                  src="https://storage.googleapis.com/storage.chris-chris.ai/images/twitter.gif"
-                  title="트위터로 공유하기"
-                  class="sharebtn_custom"
-              /></a>
-              <a href=""
-                ><img
-                  src="https://storage.googleapis.com/storage.chris-chris.ai/images/story.gif"
-                  title="카카오스토리로 공유하기"
-                  class="sharebtn_custom"
-              /></a>
-            </div>
-            <hr />
           </b-col>
           <b-col cols="4" class="mt-4 box"> </b-col>
         </b-row>
@@ -66,39 +80,17 @@
 </template>
 
 <script>
+import hashTag from "./item/hashTag.vue";
+
 export default {
   name: "BoardDetail",
   props: ["post", "user"],
+  components: { hashTag },
   computed: {},
   methods: {},
 };
 </script>
 
-<style>
-.box {
-  border: 1px solid black;
-}
-.user-info {
-  font-size: 13px;
-}
-.userbox {
-  display: flex;
-}
-.info-right {
-  float: right;
-}
-.post-info {
-  position: relative;
-  color: rgb(145, 141, 141);
-  font-size: 12px;
-}
-.sharebtn_custom {
-  border-radius: 50%;
-  width: 30px;
-  margin-right: 3px;
-  float: right;
-}
-.info-bottom {
-  position: relative;
-}
+<style lang="scss">
+@import '@/assets/scss/components/boardDetail.scss';
 </style>
--- frontend/src/views/content/templates/freeboard/index.vue
+++ frontend/src/views/content/templates/freeboard/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-     <b-container>
+    <b-container>
       <div style="justify-content-md-center" class="mt-4">
         <nav-bar></nav-bar>
         <b-row
@@ -11,58 +11,51 @@
           cols-md="2"
           cols-lg="2"
         >
-          <b-col
-            class="pr-0 pl-0"
-            v-for="(title, index) in post"
-            :key="index"
-          >
+          <b-col class="pr-0 pl-0" v-for="(title, index) in post" :key="index">
             <card-list :post="post[index]"></card-list>
           </b-col>
         </b-row>
       </div>
     </b-container>
   </div>
- 
 </template>
 
 <script>
-import CardList from '../../../components/cards/CardList.vue';
-import NavBar from '../../../components/navBar.vue';
+import CardList from "../../../components/cards/CardList.vue";
+import NavBar from "../../../components/navBar.vue";
 
 export default {
   components: { CardList, NavBar },
-  name: 'FreeBoard',
+  name: "FreeBoard",
   data() {
     return {
-        post: [
-          { 
-          "index":1,
-          "title": "강원도여행 가려는데",
-          "content": "asdfdf  lkadsjflkdjflkdf ㅣㅏ어라ㅣㅓㅏㅣ어라ㅣ어라어러아ㅣ러  ㅣㅏㅁㅇ렁나렁ㅁㄴ러ㅣㅇ너리ㅏㅁㅇ너라ㅣㅓㅇㅁ나ㅣ렁나ㅣㅓ이ㅏ링너라ㅣ  ㅣㄴㅁㅇ렁나러ㅏㅣㅇㄴ러ㅣㅁㄴ리ㅏㅓㅣㅇㅁ나 ㅣㅏㅇ나러아ㅣ"
-          },
-          { 
-          "index":2,
-          "title": "롤렉스 시계 선물어때?",
-          "content": "어떻게 생각해"
-          },
-          { 
-          "index":3,
-          "title": "테스트세트스",
-          "content": "오오아아아아아"
-          },
-          { 
-          "index":4,
-          "title": "우오아아아아ㅏㅇ",
-          "content": "오오아아아아아"
-          }
-        ]
+      post: [
+        {
+          index: 1,
+          title: "강원도여행 가려는데",
+          content:
+            "asdfdf  lkadsjflkdjflkdf ㅣㅏ어라ㅣㅓㅏㅣ어라ㅣ어라어러아ㅣ러  ㅣㅏㅁㅇ렁나렁ㅁㄴ러ㅣㅇ너리ㅏㅁㅇ너라ㅣㅓㅇㅁ나ㅣ렁나ㅣㅓ이ㅏ링너라ㅣ  ㅣㄴㅁㅇ렁나러ㅏㅣㅇㄴ러ㅣㅁㄴ리ㅏㅓㅣㅇㅁ나 ㅣㅏㅇ나러아ㅣ",
+        },
+        {
+          index: 2,
+          title: "롤렉스 시계 선물어때?",
+          content: "어떻게 생각해",
+        },
+        {
+          index: 3,
+          title: "테스트세트스",
+          content: "오오아아아아아",
+        },
+        {
+          index: 4,
+          title: "우오아아아아ㅏㅇ",
+          content: "오오아아아아아",
+        },
+      ],
     };
   },
-  methods: {
-
-  },
-}
+  methods: {},
+};
 </script>
 
-<style>
-</style>
+<style></style>
