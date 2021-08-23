371f7062b6570b2d41e13dd2089b472db9652d78
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2021-08-24 00:59:04 +0900

    [FE]상세페이지 해시태그 구현

---
+++ frontend/src/views/components/item/hashTag.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mt-4">
+    <div class="tag-box" v-for="value in hashTag" :key="value">
+      <a href="#"> {{ value }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HashTag",
+  props: {
+    hashTag: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+
+<style lang="scss">
+@import '@/assets/scss/components/hashTag.scss';
+</style>
+
