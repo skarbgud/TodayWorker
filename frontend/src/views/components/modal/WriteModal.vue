33663d45f8955366ca15117dc081ffcdf78ff5f7
sjarbgud <sjarbgud@gmail.com>
2021-08-24 21:23:30 +0900

    [FE] 글쓰기 모달

--- frontend/src/views/components/modal/WriteModal.vue
+++ frontend/src/views/components/modal/WriteModal.vue
@@ -6,10 +6,11 @@
     scrollable
     no-close-on-esc
     no-close-on-backdrop
+    v-model="modalShow"
   >
     <!-- 모달 header -->
-    <template #modal-header="{ close }">
-      <b-button variant="light" size="sm">
+    <template #modal-header>
+      <b-button variant="light" size="sm" class="post-button">
         등록
       </b-button>
       <div class="title-wraper">
@@ -24,15 +25,26 @@
 
     <!-- 모달 content  -->
     <el-collapse v-model="activeNames" @change="handleCategori">
-      <el-collapse-item title="카테고리" name="categori">
+      <el-collapse-item name="categori">
+        <!-- 선택된 카테고리  -->
+        <template slot="title">
+          {{ categoriName }}
+        </template>
         <!-- 카테고리 목록 -->
-        <b-list-group>
-          <b-list-group-item variant="light" v-for="(cardTitle, index) in cardTitles" :key="index">
-            {{ cardTitle.emoticon }} {{ cardTitle.title }}
+        <b-list-group v-if="isShowCategori">
+          <b-list-group-item
+            v-for="(cardTitle, index) in cardTitles"
+            :key="index"
+            class="d-flex justify-content-between align-items-center"
+            @click="clickCategori(index)"
+          >
+            {{ cardTitle.emoticon }}
+            {{ cardTitle.title }}
           </b-list-group-item>
         </b-list-group>
       </el-collapse-item>
     </el-collapse>
+    <textarea></textarea>
   </b-modal>
 </template>
 
@@ -43,13 +55,29 @@ export default {
   name: 'LoginModal',
   data() {
     return {
+      modalShow: false,
       activeNames: [],
+      categoriName: '카테고리',
       cardTitles,
+      isShowCategori: false,
     };
   },
   methods: {
-    handleCategori(val) {
-      console.log(val);
+    // 모달 창 닫기 => 선택된 카테고리 초기화
+    close() {
+      this.modalShow = false;
+      this.activeNames = [];
+      this.categoriName = '카테고리';
+    },
+    // 카테고리 목록 보이기
+    handleCategori() {
+      this.isShowCategori = true;
+    },
+    // 카테고리 클릭 => 선택된 카테고리를 collapse 타이틀로
+    clickCategori(index) {
+      this.isShowCategori = false;
+      this.activeNames = [];
+      this.categoriName = this.cardTitles[index].title;
     },
   },
 };
