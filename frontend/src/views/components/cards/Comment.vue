16e4cfc391dbf55306ab17a6080a7878153da8df
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2021-08-29 01:01:01 +0900

    [FE]대댓글구현, (TODO 대댓글 그룹별로 묶어서 끼워넣는것)

--- frontend/src/views/components/cards/Comment.vue
+++ frontend/src/views/components/cards/Comment.vue
@@ -2,19 +2,46 @@
   <div class="mt-4">
     <b-container>
       <div v-for="(item, index) in comments" :key="index" class="mt-3 col-8">
-        <div v-if="item.class === 0">
-          <div class="mt-2 comment-user">{{ item.company }} | {{ item.userId }} </div>
+        <!--본댓글 -->
+        <div :class="{ isRecomment: item.isRecomment === true }">
+          <div class="mt-2 comment-user">
+            {{ item.company }} | {{ item.userId }}
+          </div>
           <div class="mt-2">{{ item.content }}</div>
           <div class="mt-2 comment-bottom">
-            <b-icon class="mx-1" icon="clock" />2시간전
-            <a href="#"><b-icon class="mx-1 ml-1" icon="hand-thumbs-up" />좋아요</a>
+            <b-icon class="mx-1" icon="clock" />2시간전 {{ item.isRecomment }}
+            <a href="#"
+              ><b-icon class="mx-1 ml-1" icon="hand-thumbs-up" />좋아요</a
+            >
             <a href="#"><b-icon class="mx-1 ml-2" icon="chat" />1</a>
           </div>
           <hr />
         </div>
       </div>
-      <div>
-        대댓글
+      <div class="recomment-hide" @click="moreComments = !moreComments">
+        + 대댓글 {{ recommentSelect.length }} 개 더 보기
+        <hr />
+      </div>
+      <div v-if="moreComments">
+        <div v-for="(item, index) in comments" :key="index" class="mt-3 col-8">
+          <div
+            v-if="item.isRecomment"
+            :class="{ isRecomment: item.isRecomment === true }"
+          >
+            <div class="mt-2 comment-user">
+              {{ item.company }} | {{ item.userId }}
+            </div>
+            <div class="mt-2">{{ item.content }}</div>
+            <div class="mt-2 comment-bottom">
+              <b-icon class="mx-1" icon="clock" />2시간전 {{ item.isRecomment }}
+              <a href="#"
+                ><b-icon class="mx-1 ml-1" icon="hand-thumbs-up" />좋아요</a
+              >
+              <a href="#"><b-icon class="mx-1 ml-2" icon="chat" />1</a>
+            </div>
+            <hr />
+          </div>
+        </div>
       </div>
     </b-container>
   </div>
@@ -24,11 +51,31 @@
 export default {
   name: "Comment",
   props: ["comments"],
-  computed: {},
+  computed: {
+    recommentSelect() {
+      return this.comments.filter((item) => item.isRecomment);
+    },
+  },
+  created() {
+    this.commentSort();
+  },
+  data() {
+    return {
+      moreComments: false,
+      comment: undefined,
+    };
+  },
+  methods: {
+    //코멘트 댓글그룹별 묶어서 정렬
+    commentSort() {
+      this.comment = this.comments.sort(function(a, b) {
+        return a.groupNum < b.groupNum ? -1 : a.groupNum > b.groupNum ? 1 : 0;
+      });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/components/comment.scss';
+@import "@/assets/scss/components/comment.scss";
 </style>
-
