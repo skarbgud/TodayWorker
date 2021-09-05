a363fa595cbab27005f108a8e2dcfdfdd61bf81e
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2021-09-05 19:35:56 +0900

    [FE]해쉬태그 변경

--- frontend/src/views/components/item/HashTag.vue
+++ frontend/src/views/components/item/HashTag.vue
@@ -1,24 +1,36 @@
 <template>
-  <div class="mt-4">
-    <div class="tag-box" v-for="value in hashTag" :key="value">
-      <a href="#"> {{ value }}</a>
-    </div>
+  <div class="mt-3 mb-2">
+    <a href="#">
+    <b-form-tags
+      input-id="tags-pills"
+      v-model="value"
+      tag-variant="primary"
+      tag-pills
+      size="md"
+      separator=" "
+      placeholder=""
+      add-on-change=false
+      disabled
+    />
+    </a>
   </div>
 </template>
 
 <script>
 export default {
   name: "HashTag",
-  props: {
-    hashTag: {
-      type: Object,
-    },
+  data() {
+    return {
+      value:["# aaaa","# bbb","# ccc"]
+    }
   },
+  methods:{
+  }
 };
 </script>
 
 
 <style lang="scss" scoped>
-@import '@/assets/scss/components/hashTag.scss';
+// @import '@/assets/scss/components/hashTag.scss';
 </style>
 
