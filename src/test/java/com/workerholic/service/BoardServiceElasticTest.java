503dfe40078435d259dfb4c084536ae2cf87945d
sjarbgud <sjarbgud@gmail.com>
2021-10-01 23:13:13 +0900

    [BE] @Test 엘라스틱서치 BoardListService insertBoard() [Create]

--- src/test/java/com/workerholic/service/BoardServiceElasticTest.java
+++ src/test/java/com/workerholic/service/BoardServiceElasticTest.java
@@ -1,37 +1,36 @@
 package com.workerholic.service;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.http.HttpHost;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.workerholic.mapper.BoardMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
 public class BoardServiceElasticTest {
 
 	private Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().serializeNulls().create();
-	
+
 	private String hostname = "localhost";
 	private Integer port = 9200;
 
@@ -39,29 +38,28 @@ public class BoardServiceElasticTest {
 	public RestHighLevelClient restHighLevelClient() {
 		return new RestHighLevelClient(RestClient.builder(new HttpHost(hostname, port, "http")));
 	}
-	
+
 	private final RestHighLevelClient client = restHighLevelClient();
-	
+
+	String indexName = "board";
+
 	@Test
 	public void getBoardList() {
 
-		String indexName = "board";
-		
 		List<Map<String, Object>> boardList = new ArrayList<Map<String, Object>>();
-		
+
 		// 쿼리문
 		SearchRequest searchRequest = new SearchRequest(indexName);
 		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
 		sourceBuilder.from(0);
-		sourceBuilder.size(10);
-		
+		sourceBuilder.size(1000);
+
 		searchRequest.source(sourceBuilder);
-		
+
 		try {
 			SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-			
-			for (SearchHit hit : searchResponse.getHits().getHits())
-			{
+
+			for (SearchHit hit : searchResponse.getHits().getHits()) {
 				Map<String, Object> sourceMap = hit.getSourceAsMap();
 				boardList.add(sourceMap);
 			}
@@ -69,8 +67,33 @@ public class BoardServiceElasticTest {
 			e.printStackTrace();
 		}
 
-				
 		System.out.println(gson.toJson(boardList));
+	}
+
+	@Test
+	public void insertBoard() {
+
+		Map<String, Object> boardVO = new HashMap<String, Object>();
+
+		for (int i = 0; i < 1000; i++) {
+			boardVO.put("bno", i);
+			boardVO.put("boardType", "live" + i);
+			boardVO.put("title", "테스트제목" + i);
+			boardVO.put("content", "테스트내용" + i);
+			boardVO.put("cnt", i);
+			boardVO.put("writer", "testUser" + i);
+			boardVO.put("registDate", new Date());
+
+			try {
+				IndexRequest request = new IndexRequest(indexName).source(boardVO);
+
+				client.index(request, RequestOptions.DEFAULT);
+
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
