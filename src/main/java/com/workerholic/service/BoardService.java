499454ad989c3d6920348ffa8087443d893be0ca
sjarbgud <sjarbgud@gmail.com>
2021-10-02 23:51:27 +0900

    [BE] BoardService getBoardList() 구현, ElasticsearchConnection 연결 설정

--- src/main/java/com/workerholic/service/BoardService.java
+++ src/main/java/com/workerholic/service/BoardService.java
@@ -4,28 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.HttpHost;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import com.workerholic.mapper.BoardMapper;
+import com.workerholic.utils.ElasticsearchConnect;
 
 @Service
 public class BoardService implements BoardServiceIF {
 
-	@Autowired
-	private SqlSessionFactory sqlSessionFactory;
+	// es연결 정보
+	ElasticsearchConnect connect = new ElasticsearchConnect("localhost", 9200);
+
+	// Rest connection 설정
+	private final RestHighLevelClient client = connect.getConnection();
+
+	// 인덱스 name
+	private String indexName = "board";
 
 	public List<Map<String, Object>> getBoardList() throws Exception {
-		
 		List<Map<String, Object>> boardList = new ArrayList<Map<String, Object>>();
 
-		SqlSession session = sqlSessionFactory.openSession();
-		
-		BoardMapper mapper = session.getMapper(BoardMapper.class);
+		// 쿼리문
+		SearchRequest searchRequest = new SearchRequest(indexName);
+		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+		// 시작위치
+		sourceBuilder.from(0);
+		// 가져오는 데이터양
+		sourceBuilder.size(1000);
+		searchRequest.source(sourceBuilder);
 
-		boardList = mapper.getBoardList();
+		try {
+			SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+
+			for (SearchHit hit : searchResponse.getHits().getHits()) {
+				Map<String, Object> sourceMap = hit.getSourceAsMap();
+				boardList.add(sourceMap);
+			}
+		} 
+		catch (ElasticsearchStatusException e) 
+		{
+			e.printStackTrace();
+		}
 
 		return boardList;
 	}
