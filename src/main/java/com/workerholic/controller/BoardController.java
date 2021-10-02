a11f5b05c00ddf7578659f56e88ff9a3ba680c2e
sjarbgud <sjarbgud@gmail.com>
2021-10-03 00:18:17 +0900

    [BE] BoardController 인터페이스 생성 및 구현

--- src/main/java/com/workerholic/controller/BoardController.java
+++ src/main/java/com/workerholic/controller/BoardController.java
@@ -4,16 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.workerholic.service.BoardService;
+import com.workerholic.service.BoardServiceIF;
+import com.workerholic.vo.BoardVO;
 import com.workerholic.vo.ResultVO;
 
 @Controller
 @RequestMapping("board/")
-public class BoardController {
+public class BoardController implements BoardControllerIF {
 
 	private static final Logger LOG = LoggerFactory.getLogger(BoardController.class);
 
@@ -29,9 +32,73 @@ public class BoardController {
 		try {
 			result.setData(service.getBoardList());
 			result.setSuccess(true);
-		} 
-		catch (Exception e) {
+		} catch (Exception e) {
 			LOG.error("[Board] getBoardList : " + e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "get-board-detail.do", method = RequestMethod.POST)
+	public ResultVO getBoardDetail(@RequestBody BoardVO vo) {
+		LOG.info("GetBoardDetail");
+		ResultVO result = new ResultVO(false, null);
+
+		try {
+			result.setData(service.getBoardDetail());
+			result.setSuccess(true);
+		} catch (Exception e) {
+			LOG.error("[Board] getBoardDetail : " + e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "insert-board.do", method = RequestMethod.POST)
+	public ResultVO insertBoard(@RequestBody BoardVO vo) {
+		LOG.info("InsertBoard");
+		ResultVO result = new ResultVO(false, null);
+
+		try {
+			service.insertBoard(vo);
+			result.setSuccess(true);
+		} catch (Exception e) {
+			LOG.error("[Board] insertBoard : " + e.getMessage(), e);
+		}
+
+		return result;
+
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "update-board.do", method = RequestMethod.POST)
+	public ResultVO updateBoard(@RequestBody BoardVO vo) {
+		LOG.info("UpdateBoard");
+		ResultVO result = new ResultVO(false, null);
+
+		try {
+			service.updateBoard(vo);
+			result.setSuccess(true);
+		} catch (Exception e) {
+			LOG.error("[Board] updaeteBoard : " + e.getMessage(), e);
+		}
+
+		return result;
+
+	}
+
+	@Override
+	public ResultVO deleteBoard(BoardVO vo) {
+		LOG.info("DeleteBoard");
+		ResultVO result = new ResultVO(false, null);
+
+		try {
+			service.deleteBoard(vo);
+			result.setSuccess(true);
+		} catch (Exception e) {
+			LOG.error("[Board] deleteBoard : " + e.getMessage(), e);
 		}
 
 		return result;
