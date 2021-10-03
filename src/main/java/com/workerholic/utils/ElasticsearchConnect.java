2d54db74b1b1c85d223a72711c1f175b30a6230d
sjarbgud <sjarbgud@gmail.com>
2021-10-03 21:19:47 +0900

    [BE] CrossOrigin, ElasticSearch 아이디 패스워드 설정

--- src/main/java/com/workerholic/utils/ElasticsearchConnect.java
+++ src/main/java/com/workerholic/utils/ElasticsearchConnect.java
@@ -5,9 +5,11 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
 
 import lombok.Data;
 
@@ -16,55 +18,53 @@ public class ElasticsearchConnect {
 
 	private String address = "localhost";
 	private Integer port = 9200;
-	private String userId = "elastic";
-	private String password = "elastic";
-	
-	public ElasticsearchConnect(String address, Integer port, String userId, String password) 
-	{
-		if(address != null)
-		{
+	private String userId = "newadmin";
+	private String password = "password123";
+
+	public ElasticsearchConnect(String address, Integer port, String userId, String password) {
+		if (address != null) {
 			setAddress(address);
 		}
-		
-		if(port != null)
-		{
+
+		if (port != null) {
 			setPort(port);
 		}
-		
-		if(userId != null)
-		{
+
+		if (userId != null) {
 			setUserId(userId);
 		}
-		
-		if(password != null)
-		{
+
+		if (password != null) {
 			setPassword(password);
 		}
 	}
-	
-	public ElasticsearchConnect(String address, Integer port) 
-	{
-		if(address != null)
-		{
+
+	public ElasticsearchConnect(String address, Integer port) {
+		if (address != null) {
 			setAddress(address);
 		}
-		
-		if(port != null)
-		{
+
+		if (port != null) {
 			setPort(port);
 		}
 	}
-	
-	public CredentialsProvider getCredentialsProvider()
-	{
+
+	public CredentialsProvider getCredentialsProvider() {
 		CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-		
+
 		credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(getUserId(), getPassword()));
-		
+
 		return credentialsProvider;
 	}
-		
+
 	public RestHighLevelClient getConnection() {
-		return new RestHighLevelClient(RestClient.builder(new HttpHost(this.address, this.port, "http")));
+		return new RestHighLevelClient(RestClient.builder(new HttpHost(this.address, this.port, "http"))
+				.setHttpClientConfigCallback(new HttpClientConfigCallback() {
+					@Override
+					public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+						// TODO Auto-generated method stub
+						return httpClientBuilder.setDefaultCredentialsProvider(getCredentialsProvider());
+					}
+				}));
 	}
 }
