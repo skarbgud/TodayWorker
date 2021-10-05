f5c2423a1c3d070784a73bc3fcf6679e926babd8
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2021-10-05 20:39:56 +0900

    [FE]카드리스트에 백엔드 데이터 연동

--- frontend/src/views/components/cards/CardList.vue
+++ frontend/src/views/components/cards/CardList.vue
@@ -13,15 +13,15 @@
 
       <!--유저 정보-->
       <div class="user-info">
-        <a href="#" class="card-link">삼성전자</a>
-        <b-link href="#" class="card-link">m****</b-link><br />
+        <a href="#" class="card-link">새회사</a>
+        <b-link href="#" class="card-link">{{post.writer}}</b-link><br/>
       </div>
 
       <!-- 조회수, 좋아요, 댓글 갯수-->
       <div class="icon-color mt-1">
-        <b-icon class="ml-0 " icon="hand-index-thumb" /> 505
+        <b-icon class="ml-0 mx-2" icon="hand-index-thumb" />{{post.cnt}}
         <b-icon class="mx-2 ml-3 " icon="hand-thumbs-up" />좋아요
-        <b-icon class="mx-2 ml-3 " icon="chat" />38
+        <b-icon class="mx-2 ml-3 " icon="chat" />{{post.recomment}}
       </div>
     </b-card>
   </div>
