988792a74e7f9d3fca419446b071064577ae8a94
sjarbgud <sjarbgud@gmail.com>
2021-10-08 20:56:58 +0900

    [BE] board 인덱스 삭제 후 생성(deleteAndCreateIndexData메소드 실행)

--- src/test/java/com/workerholic/service/BoardServiceElasticTest.java
+++ src/test/java/com/workerholic/service/BoardServiceElasticTest.java
@@ -10,22 +10,17 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.apache.commons.beanutils.BeanUtils;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -33,7 +28,6 @@ import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -50,7 +44,7 @@ public class BoardServiceElasticTest {
 
 	private String hostname = "localhost";
 	private Integer port = 9200;
-	
+
 	// es연결 정보
 	ElasticsearchConnect connect = new ElasticsearchConnect("localhost", 9200);
 
@@ -86,36 +80,35 @@ public class BoardServiceElasticTest {
 
 		System.out.println(gson.toJson(boardList));
 	}
-	
+
 	@Test
-	public void getBoardDetail()
-	{
+	public void getBoardDetail() {
 		BoardVO boardVO = new BoardVO();
 		boardVO.setBno("2672725de75d46f3868acad9cda040db");
 		Map<String, Object> board = new HashMap<String, Object>();
-		
+
 		// bno
 		String boardNumer = boardVO.getBno();
 		// id (인덱스이름 + bno)
 		String id = indexName + boardNumer;
-		
-		//쿼리문
+
+		// 쿼리문
 		SearchRequest searchRequest = new SearchRequest(indexName);
 		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
 		BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
 		boolQueryBuilder.must(QueryBuilders.termQuery("_id", id));
 		sourceBuilder.query(boolQueryBuilder);
-		
+
 		searchRequest.source(sourceBuilder);
-		
+
 		try {
-			SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);;
-			
-			for (SearchHit hit : searchResponse.getHits().getHits())
-			{
+			SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+			;
+
+			for (SearchHit hit : searchResponse.getHits().getHits()) {
 				board = hit.getSourceAsMap();
-			}			
-			
+			}
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -138,7 +131,7 @@ public class BoardServiceElasticTest {
 			boardVO.put("registDate", new Date());
 
 			try {
-				IndexRequest request = new IndexRequest(indexName).id(indexName+i).source(boardVO);
+				IndexRequest request = new IndexRequest(indexName).id(indexName + i).source(boardVO);
 
 				client.index(request, RequestOptions.DEFAULT);
 
@@ -148,15 +141,14 @@ public class BoardServiceElasticTest {
 			}
 		}
 	}
-	
-	
+
 	@Test
 	public void insertBoardUUID() {
 		BoardVO boardVO = new BoardVO();
 		boardVO.setTitle("UUID제목테스트");
 		boardVO.setContent("UUID내용테스트");
 		boardVO.setType("live");
-		
+
 		try {
 			insertBoardUUIDMethod(boardVO);
 		} catch (IOException e) {
@@ -164,18 +156,18 @@ public class BoardServiceElasticTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void insertBoardUUIDMethod(BoardVO boardVO) throws IOException {
 		String uuid = UUID.randomUUID().toString();
 		uuid = uuid.replace("-", "");
-		
+
 		// bno는 uuid로 고유화
 		boardVO.setBno(uuid);
 		// 조회수는 기본값 0
 		boardVO.setCnt(0);
 		// 현재 날짜
 		boardVO.setRegdate(new Date());
-		
+
 		Map<String, String> boardMap = null;
 		try {
 			boardMap = BeanUtils.describe(boardVO);
@@ -183,37 +175,38 @@ public class BoardServiceElasticTest {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		try {
 			// id는 인덱스 + bno로 unique
-			IndexRequest request = new IndexRequest(indexName).id(indexName+boardVO.getBno()).source(boardMap, XContentType.JSON);
-			
+			IndexRequest request = new IndexRequest(indexName).id(indexName + boardVO.getBno()).source(boardMap,
+					XContentType.JSON);
+
 			client.index(request, RequestOptions.DEFAULT);
-			
+
 		} catch (ElasticsearchStatusException e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void updateBoard() {
-		
+
 		int bno = 1;
-		
+
 		Map<String, Object> boardVO = new HashMap<String, Object>();
-		
+
 		boardVO.put("bno", bno);
 		boardVO.put("boardType", "live");
 		boardVO.put("title", "변경테스트제목");
 		boardVO.put("content", "변경테스트내용");
 		boardVO.put("cnt", 1);
-		boardVO.put("writer", "변경User" );
+		boardVO.put("writer", "변경User");
 		boardVO.put("registDate", new Date());
-		
+
 		try {
-			UpdateRequest request = new UpdateRequest(indexName, indexName+bno).doc(boardVO);
-			
+			UpdateRequest request = new UpdateRequest(indexName, indexName + bno).doc(boardVO);
+
 			client.update(request, RequestOptions.DEFAULT);
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -223,13 +216,28 @@ public class BoardServiceElasticTest {
 
 	@Test
 	public void deleteBoard() {
-		int bno = 1; 
-		DeleteRequest request = new DeleteRequest(indexName, indexName+bno);
-		
+		int bno = 1;
+		DeleteRequest request = new DeleteRequest(indexName, indexName + bno);
+
 		try {
 			client.delete(request, RequestOptions.DEFAULT);
 		} catch (Exception e) {
 			// TODO: handle exception
+		}
+	}
+
+	@Test
+	public void deleteAndCreateIndexData() {
+		DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest(indexName);
+		CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
+
+		createIndexRequest.settings(Settings.builder().put("index.number_of_shards", 1).put("index.number_of_replicas", 0));
+
+		try {
+			client.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
+			client.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
