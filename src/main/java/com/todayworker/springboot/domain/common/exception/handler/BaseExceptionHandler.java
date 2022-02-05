462326353cf213a41266b527d2291f6bd60e41d5
GyuHyeong Nam <sjarbgud@gmail.com>
2022-02-05 21:14:18 +0900

    [BE] Error에 대한 로그 남기기

--- src/main/java/com/todayworker/springboot/domain/common/exception/handler/BaseExceptionHandler.java
+++ src/main/java/com/todayworker/springboot/domain/common/exception/handler/BaseExceptionHandler.java
@@ -3,25 +3,30 @@ package com.todayworker.springboot.domain.common.exception.handler;
 import com.todayworker.springboot.domain.common.dto.BaseErrorResponse;
 import com.todayworker.springboot.domain.common.exception.BaseException;
 import com.todayworker.springboot.domain.common.exception.enums.BaseErrorCode;
-import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
+@Slf4j
 public class BaseExceptionHandler {
 
     @ExceptionHandler({BaseException.class})
     @ResponseBody
     public ResponseEntity<BaseErrorResponse> handleCommonException(
         BaseException e, HttpServletRequest request) {
+    	log.warn(e.toString());
         return new ResponseEntity(BaseErrorResponse.createErrorResponse(e), e.getErrorStatus());
     }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<BaseErrorResponse> handleException(
         Exception e, HttpServletRequest request) {
+    	log.error(e.toString());
         request.getSession();
 
         BaseException boxedException = new BaseException(BaseErrorCode.UNKNOWN_SERVER_ERROR, e);
